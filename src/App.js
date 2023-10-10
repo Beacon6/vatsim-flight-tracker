ed53382d6701662d3cdf770eaf59b90b7e5f6477
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-10 22:59:44 +0200

    Start frontend integration

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
-import { Map } from "pigeon-maps"
+import { Map, Marker } from "pigeon-maps"
 import { maptiler } from 'pigeon-maps/providers'
 
 const maptilerProvider = maptiler('KR6OCyEjZD3WgFTla3Rv', 'dataviz')
 
-export function MyMap() {
+function App() {
+
+  const [aircraftState, setAircraftState] = useState([]);
+
+  useEffect(() => {
+    fetch('/state')
+    .then(response => response.json())
+    .then(data => setAircraftState(data))
+  }, []);
+
   return (
-    <div style={{height: '100vh'}}>
-    <Map
-    provider={maptilerProvider}
-    dprs={[1, 2]} // this provider supports HiDPI tiles
-    defaultCenter={[50, 10]}
-    defaultZoom={6}
-  />
+  <div style={{height: '100vh'}}>
+    <Map provider={maptilerProvider} dprs={[1, 2]} defaultCenter={[50, 10]} defaultZoom={6}>
+      <Marker
+        width={50}
+        anchor={[50, 10]}
+      />
+    </Map>
   </div>
   )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default MyMap;
+export default App;
