9c11ff3d0b18a3e9fd61122d7f8edbf45377c5c4
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-10 21:48:39 +0200

    Modify Python backend to collect aircraft data

--- api.py
+++ api.py
@@ -1,12 +1,36 @@
 from flask import Flask
 from opensky_api import OpenSkyApi # type: ignore
-
-api = OpenSkyApi()
-s = api.get_states()
-print(s)
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/state')
+@app.route("/state")
 def draw_state():
-    return
+    api = OpenSkyApi("Beacon6", "FE01D81011737B8704E1256D24F2C2C16B3083BC2F1C5A90106C80BE24F40E20")
+
+    # bbox = (min latitude, max latitude, min longitude, max longitude)
+    open_sky_states = api.get_states(bbox=(45.8389, 47.8229, 5.9962, 10.5226))
+
+    tracked_aircraft = []
+
+    for x in open_sky_states.states:
+        # fetch: icao24, callsign, longitude, latitude, on_ground, velocity, true_track,
+        # vertical_rate, baro_altitude, squawk
+
+        tracked_aircraft.append({
+            "callsign" : x.callsign,
+            "longitude" : x.longitude,
+            "latitude" : x.latitude,
+            "velocity" : x.velocity,
+            "true_track" : x.true_track,
+            "vertical_rate" : x.vertical_rate,
+            "baro_altitude" : x.baro_altitude,
+            "on_ground" : x.on_ground,
+            "squawk": x.squawk
+            })
+
+    df = pd.DataFrame(tracked_aircraft)
+    return df
+
+df = draw_state()
+print(df)
