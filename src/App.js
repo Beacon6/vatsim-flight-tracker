27257c38e7ff37485bc105ad83cee1b026e8d90f
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-11 02:38:37 +0200

    Change map properties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { Map, Marker } from "pigeon-maps"
+import { Map, Marker, ZoomControl } from "pigeon-maps"
 import { maptiler } from 'pigeon-maps/providers'
 
 const maptilerProvider = maptiler('KR6OCyEjZD3WgFTla3Rv', 'dataviz')
@@ -17,7 +17,8 @@ function App() {
 
   return (
   <div style={{height: '100vh'}}>
-    <Map provider={maptilerProvider} dprs={[1, 2]} defaultCenter={[50, 10]} defaultZoom={6}>
+    <Map provider={maptilerProvider} dprs={[1, 2]} defaultCenter={[50, 10]} defaultZoom={6} minZoom={4} >
+      <ZoomControl/>
       <Marker
         width={50}
         anchor={[50, 10]}
