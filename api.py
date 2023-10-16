a8353f2cbe7cda8a52bb0e4d072de3cef62d8291
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-16 15:38:36 +0200

    Rework Python backend to get flight data on each frontend call

--- api.py
+++ api.py
@@ -4,11 +4,12 @@ from opensky_api import OpenSkyApi # type: ignore
 app = Flask(__name__)
 api = OpenSkyApi("Beacon6", "FE01D81011737B8704E1256D24F2C2C16B3083BC2F1C5A90106C80BE24F40E20")
 
-aircraft_data = []
-
-def get_aircraft_data():
+@app.route("/aircraft_data")
+def get_aircraft_data(min_lat, max_lat, min_lon, max_lon):
     # bbox = (min latitude, max latitude, min longitude, max longitude)
-    aircraft_states = api.get_states(bbox=(47.26, 54.92, 5.34, 14.90))
+    aircraft_states = api.get_states(bbox = (min_lat, max_lat, min_lon, max_lon))
+
+    aircraft_data = []
 
     for s in aircraft_states.states:
         aircraft_data.append({
@@ -18,10 +19,5 @@ def get_aircraft_data():
             "latitude" : s.latitude,
             })
 
-    return aircraft_data
-
-aircraft_data = get_aircraft_data()
-
-@app.route("/aircraft_data")
-def send_aircraft_data():
+    print(len(aircraft_data))
     return aircraft_data
