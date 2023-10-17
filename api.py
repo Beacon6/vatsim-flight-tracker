b0e9e81ecdf47235f7ed3b16e739be86375d2552
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-17 14:46:26 +0200

    Refactor Python backend

--- api.py
+++ api.py
@@ -1,28 +1,30 @@
 from flask import Flask, request
-from opensky_api import OpenSkyApi # type: ignore
+from opensky_api import OpenSkyApi  # type: ignore
 
 app = Flask(__name__)
-api = OpenSkyApi("Beacon6", "FE01D81011737B8704E1256D24F2C2C16B3083BC2F1C5A90106C80BE24F40E20")
+key = "FE01D81011737B8704E1256D24F2C2C16B3083BC2F1C5A90106C80BE24F40E20"
+api = OpenSkyApi("Beacon6", key)
 
-@app.route("/aircraft_data", methods = ["POST"])
+
+@app.route("/aircraft_data", methods=["POST"])
 def get_aircraft_data():
     viewport_bounds = request.json
     print(viewport_bounds)
 
     # bbox = (min latitude, max latitude, min longitude, max longitude)
-    aircraft_states = api.get_states(bbox = (viewport_bounds["sw"][0],
-                                             viewport_bounds["ne"][0],
-                                             viewport_bounds["sw"][1],
-                                             viewport_bounds["ne"][1]))
+    aircraft_states = api.get_states(bbox=(viewport_bounds["sw"][0],
+                                           viewport_bounds["ne"][0],
+                                           viewport_bounds["sw"][1],
+                                           viewport_bounds["ne"][1]))
 
     aircraft_data = []
 
     for s in aircraft_states.states:
         aircraft_data.append({
-            "icao24" : s.icao24,
-            "callsign" : s.callsign,
-            "longitude" : s.longitude,
-            "latitude" : s.latitude,
+            "icao24": s.icao24,
+            "callsign": s.callsign,
+            "longitude": s.longitude,
+            "latitude": s.latitude,
             })
 
     print(len(aircraft_data))
