44c44bb2ec6077df73705d2cb91965be7badf54b
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2023-10-31 14:52:51 +0100

    Add additional data to the API call

--- api.py
+++ api.py
@@ -8,11 +8,11 @@ api = OpenSkyApi("Beacon6", key)
 
 debug = True
 dummy_data = [
-    {'icao24': '4b1814', 'callsign': 'SWR3YZ  ', 'longitude': 3.7492, 'latitude': 48.5161},
-    {'icao24': '4b1815', 'callsign': 'SWR85Q  ', 'longitude': -0.3495, 'latitude': 51.3015},
-    {'icao24': '4b1803', 'callsign': 'SWR6PC  ', 'longitude': 17.3085, 'latitude': 47.5221},
-    {'icao24': '4b1806', 'callsign': 'SWR797  ', 'longitude': 5.974, 'latitude': 49.8146},
-    {'icao24': '4b1807', 'callsign': 'SWR81AT ', 'longitude': 16.5687, 'latitude': 48.1186}]
+    {'icao24': '4b1816', 'callsign': 'SWR8WA', 'longitude': 14.9186, 'latitude': 45.6102, 'velocity': 237.93, 'true_track': 130.44, 'baro_altitude': 11277.6, 'category': 0},
+    {'icao24': '440da4', 'callsign': 'JPV137', 'longitude': 9.5135, 'latitude': 47.6696, 'velocity': 0, 'true_track': 140.62, 'baro_altitude': None, 'category': 0},
+    {'icao24': '4b1804', 'callsign': 'SWR1ZH', 'longitude': 14.4882, 'latitude': 46.208, 'velocity': 69.86, 'true_track': 306.62, 'baro_altitude': 510.54, 'category': 0},
+    {'icao24': '4b1806', 'callsign': 'SWR6LH', 'longitude': 6.3414, 'latitude': 46.396, 'velocity': 86.94, 'true_track': 225.24, 'baro_altitude': 1783.08, 'category': 0},
+    {'icao24': '4b1807', 'callsign': 'SWR6FL', 'longitude': 6.7643, 'latitude': 51.2833, 'velocity': 10.29, 'true_track': 53.44, 'baro_altitude': None, 'category': 0}]
 
 
 @app.route("/aircraft_data", methods=["POST"])
@@ -39,6 +39,10 @@ def get_aircraft_data():
                     "callsign": s.callsign,
                     "longitude": s.longitude,
                     "latitude": s.latitude,
+                    "velocity": s.velocity,
+                    "true_track": s.true_track,
+                    "baro_altitude": s.baro_altitude,
+                    "category": s.category
                     })
 
         aircraft_count = len(aircraft_data)
