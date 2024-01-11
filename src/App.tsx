a8b4c03ea5952e97e8283263721543f094a6e2bf
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-01-11 18:32:51 +0100

    Add React Leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState, useRef, SetStateAction } from 'react';
-import { Map, Source, Layer } from 'react-map-gl';
-// import airplane_icon from './airplane.png';
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import Navbar from './Navbar';
-import Panel from './Panel';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-
-// Mapbox GL API token
-const api_key = import.meta.env.VITE_API_KEY;
 
 function App() {
-  // Updating viewport bounds on each change
-  const mapRef: any = useRef();
-  const [viewportBounds, setViewportBounds] = useState();
-
-  function onBoundsChanged() {
-    const bounds = mapRef.current.getBounds();
-    const boundsObject: any = {};
-
-    boundsObject['sw'] = [bounds['_sw']['lat'], bounds['_sw']['lng']];
-    boundsObject['ne'] = [bounds['_ne']['lat'], bounds['_ne']['lng']];
-
-    setViewportBounds(boundsObject);
-
-    if (timer < 20) {
-      setRequestAllowed(true);
-      setTimer(30);
-    }
-  }
-
-  // Measuring time between API calls
+  // API call timer
   const [timer, setTimer] = useState(30);
   const [requestAllowed, setRequestAllowed] = useState(true);
 
@@ -40,33 +14,24 @@ function App() {
     }, 1000);
 
     if (timer === 0) {
-      setTimer(30);
+      setTimer(15);
       setRequestAllowed(true);
     }
 
     return () => clearInterval(interval);
   }, [timer]);
 
-  // Fetching aircraft data from the Flask backend
+  // Fetching VATSIM aircraft data from the Flask backend
   const [aircraftData, setAircraftData] = useState<any>([]);
   const [trackedCount, setTrackedCount] = useState(0);
 
   useEffect(() => {
-    if (!viewportBounds) return;
-
     if (requestAllowed === true) {
-      console.log('Fetching aircraft data for:');
-      console.log(viewportBounds);
-
-      fetch('http://localhost:5000/aircraft_data', {
-        method: 'POST',
-        body: JSON.stringify(viewportBounds),
-        headers: { 'Content-Type': 'application/json' },
-      })
+      fetch('http://localhost:5000/aircraft_data')
         .then((response) => response.json())
         .then((data) => {
           if (data.request_successful === true) {
-            setAircraftData(data.aircraft_data);
+            setAircraftData(data.aircraft_data.pilots);
             setTrackedCount(data.tracked_aircraft_count);
           } else {
             console.log('API timeout');
@@ -74,94 +39,58 @@ function App() {
         });
       setRequestAllowed(false);
     }
-  }, [viewportBounds, requestAllowed]);
+  }, [requestAllowed]);
 
-  // Handling the 'Offcanvas' panel component
-  // Fetching aircraft photo and other data for the panel
-  const [selectedAircraft, setSelectedAircraft] = useState<any>();
-  const [showPanel, setShowPanel] = useState(false);
-  const [aircraftPhoto, setAircraftPhoto] = useState<any>();
+  // // Handling the 'Offcanvas' panel component
+  // // Fetching aircraft photo and other data for the panel
+  // const [selectedAircraft, setSelectedAircraft] = useState<any>();
+  // const [showPanel, setShowPanel] = useState(false);
+  // const [aircraftPhoto, setAircraftPhoto] = useState<any>();
 
-  function handleShowPanel(selectedAircraft: SetStateAction<any>) {
-    setSelectedAircraft(selectedAircraft);
-    fetch('http://localhost:5000/aircraft_photo', {
-      method: 'POST',
-      body: JSON.stringify(selectedAircraft.icao24),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.aircraft_photo_exists === true) {
-          setAircraftPhoto(data.aircraft_photo);
-        } else {
-          console.log('Could not find any photo for this aircraft');
-        }
-      });
-    setShowPanel(true);
-    console.log('Clicked on:', selectedAircraft);
-  }
+  // function handleShowPanel(selectedAircraft: SetStateAction<any>) {
+  //   setSelectedAircraft(selectedAircraft);
+  //   fetch('http://localhost:5000/aircraft_photo', {
+  //     method: 'POST',
+  //     body: JSON.stringify(selectedAircraft.icao24),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data.aircraft_photo_exists === true) {
+  //         setAircraftPhoto(data.aircraft_photo);
+  //       } else {
+  //         console.log('Could not find any photo for this aircraft');
+  //       }
+  //     });
+  //   setShowPanel(true);
+  //   console.log('Clicked on:', selectedAircraft);
+  // }
 
-  const handleClosePanel: any = () => {
-    setShowPanel(false);
-  };
-
-  const layerStyle: any = {
-    id: 'point',
-    type: 'circle',
-    paint: {
-      'circle-radius': 10,
-      'circle-color': '#007cbf',
-    },
-  };
+  // const handleClosePanel: any = () => {
+  //   setShowPanel(false);
+  // };
 
   return (
-    <div className='app'>
-      <Navbar
-        countTotal={trackedCount}
-        countView={aircraftData.length}
-        timer={timer}
-        variant={timer < 20 ? 'success' : 'danger'}
-      />
-      <div className='map-container'>
-        <Map
-          mapboxAccessToken={api_key}
-          initialViewState={{
-            latitude: 50,
-            longitude: 10,
-            zoom: 4,
-          }}
-          mapStyle='mapbox://styles/mapbox/streets-v12'
-          onMoveEnd={onBoundsChanged}
-          ref={mapRef}
-        >
-          <Source id='my-data' type='geojson' data={aircraftData}>
-            <Layer {...layerStyle} />
-          </Source>
-          {/* {aircraftData.slice(0, 1000).map(item => (
-            <Marker
-              latitude={item.geometry.coordinates[1]}
-              longitude={item.geometry.coordinates[0]}
-              key={item.properties.icao24}
-              onClick={() => handleShowPanel(item.properties)}
-            >
-              <img
-                className='airplane-icon'
-                style={{ transform: `rotate(${item.properties.true_heading}deg)` }}
-                src={airplane_icon}
-                alt=''
-              />
-            </Marker>
-          ))} */}
-          <Button onClick={handleShowPanel} />
-        </Map>
-      </div>
-      <Panel
-        show={showPanel}
-        onHide={handleClosePanel}
-        selectedAircraftData={selectedAircraft}
-        selectedAircraftPhoto={aircraftPhoto}
-      />
-    </div>
+    <>
+      <Navbar countTotal={trackedCount} />
+      <MapContainer
+        className='app'
+        center={[50, 10]}
+        zoom={4}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        ></TileLayer>
+        {aircraftData.map((item: any) => (
+          <Marker
+            position={[item.latitude, item.longitude]}
+            key={item.cid}
+          ></Marker>
+        ))}
+      </MapContainer>
+    </>
   );
 }
 
