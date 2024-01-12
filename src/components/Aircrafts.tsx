0bec1db9c6d80a5145c1494d69cf661d13b6d486
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-01-12 18:38:46 +0100

    Add updating viewport bounds on each change

--- src/components/Aircrafts.tsx
+++ src/components/Aircrafts.tsx
@@ -1,9 +1,26 @@
 import { Marker } from 'react-leaflet';
 import { VatsimData } from '../App';
 import { icon } from 'leaflet';
+import { useMapEvent } from 'react-leaflet';
+import { useState } from 'react';
+
+interface ViewportBounds {
+  northEast: { lat: number; lng: number };
+  southWest: { lat: number; lng: number };
+}
 
 const Aircrafts: React.FC<{ vatsimData?: VatsimData }> = (props) => {
   const { vatsimData } = props;
+  const [viewportBounds, setViewportBounds] = useState<ViewportBounds>();
+
+  const map = useMapEvent('moveend', () => {
+    const mapBounds = map.getBounds();
+    setViewportBounds({
+      northEast: mapBounds.getNorthEast(),
+      southWest: mapBounds.getSouthWest(),
+    });
+    console.log(viewportBounds);
+  });
 
   const airplaneIcon = icon({
     iconUrl: '../public/assets/airplane.png',
