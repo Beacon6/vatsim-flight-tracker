9d9aa1eafb42ca59ea86b0031cbf3b181e59f111
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-02-07 15:10:44 +0100

    Change Client info panel position and layout

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,25 +3,23 @@ import { VatsimPilot } from '../App';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-interface PanelProps {
+const Panel: React.FC<{
   show: boolean;
   onHide: () => void;
   selectedClient?: VatsimPilot['vatsimPilot'];
-}
-
-const Panel: React.FC<PanelProps> = (props) => {
+}> = (props) => {
   const { show, onHide, selectedClient } = props;
 
   return (
     <>
       <Offcanvas show={show} onHide={onHide} backdrop={false}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title className='panel'>
+          <Offcanvas.Title>
             {selectedClient?.callsign} (
             {selectedClient?.flight_plan.aircraft_short})
           </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className='panel'>
+        <Offcanvas.Body>
           <Container>
             <Row>CID: {selectedClient?.cid}</Row>
             <Row>Departure: {selectedClient?.flight_plan.departure}</Row>
