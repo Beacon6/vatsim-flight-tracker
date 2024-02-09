b52265dc8866dd7b11cee885838ff873c2c0d5f1
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-02-09 19:51:04 +0100

    Start reworking the Client info panel component

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,6 +2,7 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import { VatsimPilot } from '../App';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Panel: React.FC<{
   show: boolean;
@@ -13,17 +14,42 @@ const Panel: React.FC<{
   return (
     <>
       <Offcanvas show={show} onHide={onHide} backdrop={false}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>
+        <Offcanvas.Header closeButton={false}>
+          <Offcanvas.Title as={'h5'}>
             {selectedClient?.callsign} (
             {selectedClient?.flight_plan.aircraft_short})
+          </Offcanvas.Title>
+          <Offcanvas.Title as={'h6'}>
+            CID: {selectedClient?.cid}
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Container>
-            <Row>CID: {selectedClient?.cid}</Row>
-            <Row>Departure: {selectedClient?.flight_plan.departure}</Row>
-            <Row>Arrival: {selectedClient?.flight_plan.arrival}</Row>
+            <Row>
+              <Col>Departure</Col>
+              <Col></Col>
+              <Col>Arrival</Col>
+            </Row>
+            <Row>
+              <Col>
+                <img
+                  src='../assets/departure-light.svg'
+                  height={48}
+                  width={48}
+                />
+              </Col>
+              <Col></Col>
+              <Col>
+                <img src='../assets/arrival-light.svg' height={48} width={48} />
+              </Col>
+            </Row>
+            <Row>
+              <Col>{selectedClient?.flight_plan.departure}</Col>
+              <Col></Col>
+              <Col>{selectedClient?.flight_plan.arrival}</Col>
+            </Row>
+          </Container>
+          <Container>
             <Row>Alternate: {selectedClient?.flight_plan.alternate}</Row>
             <Row>Altitude: {selectedClient?.altitude}</Row>
             <Row>Ground speed: {selectedClient?.groundspeed}</Row>
