acdf698148985bff1ed63464bd2889136a317a65
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-02-22 17:33:51 +0100

    Modify frontend code to use WebSocket

--- src/typings/VatsimData.ts
+++ src/typings/VatsimData.ts
@@ -1,53 +1,48 @@
 export interface VatsimData {
-  requestSuccessful: boolean;
-  vatsimData: {
-    pilots: {
+  pilots: {
+    altitude: number;
+    callsign: string;
+    cid: number;
+    flight_plan?: {
+      aircraft: string;
+      aircraft_faa: string;
+      aircraft_short: string;
+      alternate: string;
       altitude: number;
-      callsign: string;
-      cid: number;
-      flight_plan?: {
-        aircraft: string;
-        aircraft_faa: string;
-        aircraft_short: string;
-        alternate: string;
-        altitude: number;
-        arrival: number;
-        assigned_transponder: number;
-        cruise_tas: number;
-        departure: string;
-        deptime: number;
-        enroute_time: number;
-        flight_rules: string;
-        fuel_time: number;
-        remarks: string;
-        revision_id: number;
-        route: string;
-      };
-      groundspeed: number;
-      heading: number;
-      latitude: number;
-      longitude: number;
-      name: string;
-      qnh_i_hq: number;
-      qnh_mb: number;
-      server: string;
-      transponder: number;
-    }[];
-    controllers: {
-      cid: number;
-      callsign: string;
-      frequency: string;
-      facility: number;
-      text_atis: string[];
-      sector?: [number, number][];
-    }[];
-  };
-  pilotsCount: number;
-  atcCount: number;
+      arrival: number;
+      assigned_transponder: number;
+      cruise_tas: number;
+      departure: string;
+      deptime: number;
+      enroute_time: number;
+      flight_rules: string;
+      fuel_time: number;
+      remarks: string;
+      revision_id: number;
+      route: string;
+    };
+    groundspeed: number;
+    heading: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+    qnh_i_hq: number;
+    qnh_mb: number;
+    server: string;
+    transponder: number;
+  }[];
+  controllers: {
+    cid: number;
+    callsign: string;
+    frequency: string;
+    facility: number;
+    text_atis: string[];
+    sector?: [number, number][];
+  }[];
 }
 
 export interface VatsimPilot {
-  vatsimPilot: VatsimData['vatsimData']['pilots'][number];
+  vatsimPilot: VatsimData['pilots'][number];
 }
 
 export interface CenterBoundaries {
