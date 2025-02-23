41b6d9a421612004ad30825397159b1699b8ec91
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2025-02-23 20:41:43 +0100

    feat: change logger config

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -2,19 +2,17 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}] ${message}`;
+    }),
+  ),
   transports: [
-    new winston.transports.File({ filename: './server/logs/error.log', level: 'error' }),
+    new winston.transports.Console(),
     new winston.transports.File({ filename: './server/logs/info.log' }),
+    new winston.transports.File({ filename: './server/logs/error.log', level: 'error' }),
   ],
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  );
-}
 
 export default logger;
